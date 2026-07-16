f2663879f79a6874f7547f4104afecd8d35a02c9
David Hofer <hofer.davi@gmail.com>
2026-07-16 19:52:09 +0200

    chore(build): pin Turbopack project root

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  turbopack: {
+    root: process.cwd()
+  },
   images: {
     remotePatterns: []
   }
